927ae724c553ecbb78af8dacffa58d88d970221a
alvarius <alvarius@lattice.xyz>
2026-03-08 13:55:34 +0800

    add reference softmax attention

--- nanochat/flash_attention.py
+++ nanochat/flash_attention.py
@@ -102,6 +102,56 @@ def _sdpa_attention(q, k, v, window_size, enable_gqa):
     return F.scaled_dot_product_attention(q, k, v, attn_mask=mask, enable_gqa=enable_gqa)
 
 # =============================================================================
+# Reference softmax attention 
+# =============================================================================
+def _reference_softmax_attn(q, k, v):
+    """
+    Softmax attention for reference
+    
+    Args:
+        q, k, v: Tensors of shape (B, T, H, D)
+
+    Returns:
+        Output tensor of shape (B, T, H, D)
+    """
+
+    # Input arrives in shape 
+    #   B=Batch size,
+    #   T=Time (number of tokens),
+    #   H=Heads (number of attention heads),
+    #   D=Dimension (dimension per attention head)
+    # because it's optimized for flash attention, which uses streaming to avoid
+    # materializing the full TxT matrix. Here we'll use the naive approach of computing
+    # each head independently, so we need to swap the dimensions of the input tensors
+    q, k, v = q.transpose(1,2), k.transpose(1,2), v.transpose(1,2) # (B, T, H, D) -> (B, H, T, D)
+
+    # Scaling factor from original "Attention is all you need" paper
+    # Without it, dot products of q @ k^T would grow in magnitude proportionally to D
+    # which would result in the softmax producing mostly ~0 or ~1 values.
+    # Scaling it down keeps softmax in a more useful range
+    scale = q.size(-1) ** -0.5
+
+    # Compute attention scores -> (B, H, T, T)
+    # Per head, every row corresponds to a query and every column to a key
+    attn = torch.matmul(q, k.transpose(-2,-1)) * scale
+
+    # Create causal mask
+    T = q.size(2) # number of tokens
+    TT = torch.ones(T, T, device=q.device, dtype=torch.bool) # TxT matrix filled with True
+    mask = torch.triu(TT, diagonal=1) # main diagonal and everything below becomes false
+
+    # Apply causal mask (every "future query" result becomes -inf)
+    attn = attn.masked_fill(mask, float('-inf'))
+
+    # Apply softmax (sum of each row will be 1)
+    attn = torch.softmax(attn, dim=-1)
+
+    # Apply attention to value tensor -> (B, H, T, D)
+    y = torch.matmul(attn, v)
+
+    return y.transpose(1, 2) # -> (B, T, H, D)
+
+# =============================================================================
 # Public API: Same interface as FA3
 # =============================================================================
 def flash_attn_func(q, k, v, causal=False, window_size=(-1, -1)):
